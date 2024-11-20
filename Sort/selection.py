cf4d2cf7bc5a2a8e13f7020a7023886d34616fb6
Ceyrocx <c3dric.jeux@gmail.com>
2024-11-20 17:18:14 +0100

    ajout selction sort

--- Sort/selection.py
+++ Sort/selection.py
@@ -1,15 +1,36 @@
 from visualisationTri import visualisation
 
+
 def selection(L, time):
-    n = len(L)
+    """
+    Sorts a list using the selection sort algorithm and visualizes each step of the sorting process.
+
+    Parameters:
+    L (list): The list of elements to be sorted.
+    time (float): The time delay between visualizations, in seconds.
+
+    The selection sort algorithm repeatedly selects the smallest element from the unsorted portion
+    of the list and swaps it with the first unsorted element. This process is repeated until the list is sorted.
+    """
+
+    n = len(L)  # Get the length of the list L
+
+    # Loop over each element in the list
     for i in range(n):
-        min = L[i]
-        indMin = i
-        for j in range(i+1, n):
-            if L[j]<min:
+        min = L[i]  # Set the current element as the minimum
+        indMin = i  # Index of the minimum element
+
+        # Find the smallest element in the unsorted portion of the list
+        for j in range(i + 1, n):
+            if L[j] < min:
                 min = L[j]
                 indMin = j
+
+        # Swap the found minimum element with the current element
         L[i], L[indMin] = L[indMin], L[i]
+
+        # Visualize the current state of the list after each swap
         visualisation(L, verif=False, titre="Selection Sort", time=time)
 
+    # Final visualization when sorting is complete
     visualisation(L, verif=True, titre="Selection Sort", time=time)
