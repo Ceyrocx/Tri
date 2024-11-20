73b5d242f833c1f40e13343a6ea3bfc9ff0075b1
Ceyrocx <c3dric.jeux@gmail.com>
2024-11-20 14:56:41 +0100

    amelioration

--- Sort/bubble.py
+++ Sort/bubble.py
@@ -11,20 +11,26 @@ def bubble(L, time):
     This function repeatedly steps through the list, compares adjacent elements, and swaps them
     if they are in the wrong order. The pass through the list is repeated until the list is sorted.
     """
+
     n = len(L)  # Get the length of the list L
+    trier = False
+    j = n
 
     # Outer loop for each pass through the list
-    for j in range(n, 0, -1):
+    while j >= 1 and trier is False:
+        trier = True
         # Inner loop for comparing adjacent elements
         for i in range(1, j):
             # Compare the current element with the previous one
             if L[i] < L[i-1]:
                 # Swap elements if they are in the wrong order
                 L[i], L[i-1] = L[i-1], L[i]  # Swap the largest element to the end
+                trier = False
             # Visualize the current state of the list after each comparison
             visualisation(L, verif=False, titre="Bubble Sort", time=time)
+        j -= 1
 
     # Final visualization when sorting is complete
     visualisation(L, verif=True, titre="Bubble sort", time=time)
 
-    # return L  # Optionally return the sorted list
+    # return L  # Optionally return the sorted list (commented out)
