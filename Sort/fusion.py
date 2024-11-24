c91362615355bdc743d8afcfc1b03b5c00fd4fbb
Ceyrocx <c3dric.jeux@gmail.com>
2024-11-24 09:53:08 +0100

    suppression fichier inutile

--- Sort/fusion.py
+++
@@ -1,8 +0,0 @@
-# from visualisation import visualisation
-from random import shuffle
-
-
-L = [i for i in range(1, 101)]  # Creates a list containing integers from 1 to `amount` (inclusive).
-shuffle(L)  # Randomly shuffles the elements of the list `L` to disorder them.
-
-print(f"final : {partage(L, 0)}")
